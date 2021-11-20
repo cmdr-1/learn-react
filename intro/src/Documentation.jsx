bbc66e6aa528820d486529b02835fb87a88d9d77
Mance Rah <mance.development@gmail.com>
2021-11-20 00:42:52 -0500

    First commit

---
+++ intro/src/Documentation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Documentation() {
+    return (
+      <div>
+          <h1>This is our new component</h1>
+          <button>This is a Button</button>
+      </div>
+    );
+  }
+  
+  export default Documentation;
